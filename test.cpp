274087df3c9fcea3466e3b46eabecf24ed462d93
ran-research-lab <rafael.arce@upr.edu>
2019-08-19 15:54:16 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
 using namespace std;
+
+
+void resize(int*& A, int size) {
+  int *tmp = new int[size * 2];
+  for (int i = 0; i < size; i++) tmp[i] = A[i];
+  delete [] A;
+  A = tmp;
+}
+
 int main() {
+  int *A = new int[4];
+  for (int i = 0; i < 4; i++) A[i] = i * 10;
+  for (int i = 0; i < 4; i++) cout << A[i] << " ";
+  cout << endl;
+  resize(A,4);
+  for (int i = 0; i < 8; i++) cout << A[i] << " ";
+  cout << "\nfinito\n";
 }
